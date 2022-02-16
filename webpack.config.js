e5586e1875cb2feb2c1c2bb2c5634296ff7523d7
Justin <kaytin143@gmail.com>
2022-02-16 16:12:07 -0700

    Added nav bar

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module: {
         {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
         }
     ]
   }
