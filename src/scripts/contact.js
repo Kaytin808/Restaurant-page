9c42f161b7c8ecf7810ff0493a4e0e3daf2bf12f
Justin Moore <kaytin143@gmail.com>
2022-03-09 19:39:30 -0700

    Fixed contact src images

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,20 +1,21 @@
 var contact = document.querySelector(".navbar3");
-var gps = document.createElement("img");
 var main = document.querySelector(".main-container");
 var contactContainer = document.createElement("div");
 var phoneP = document.createElement("p");
 var detailP = document.createElement("p");
 var detailContainer = document.createElement("div");
+import addy from '../Images/address.png'
+const adGps = new Image()
 detailContainer.classList.add("detail-container");
 phoneP.innerHTML = "Customer Service : (808)-412-4583";
 detailP.innerHTML =
   "We are located in the HEART of waikiki! <br>Come on by and bring your family and friends!";
 contactContainer.classList.add("contactContainer");
-gps.classList.add("gps");
-gps.src = "../src/Images/address.png";
+adGps.classList.add("gps");
+adGps.src = addy;
 
 main.appendChild(contactContainer);
-contactContainer.appendChild(gps);
+contactContainer.appendChild(adGps);
 contactContainer.appendChild(detailContainer);
 detailContainer.appendChild(phoneP);
 detailContainer.appendChild(detailP);
